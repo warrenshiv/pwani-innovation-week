1a156b1ceb37ad2b996164e216bdffbba658baf4
warrenshiv <warrenshiv@gmail.com>
2023-09-25 10:56:09 +0300

    Implement speaker search functionality

--- src/components/Speakers/SpeakersSection.jsx
+++ src/components/Speakers/SpeakersSection.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState} from "react";
 // import '../../assets/scss/theme.scss';
 // import '../../assets/css/theme.css';
-import { speakers } from '../../data/constants';
+import { speakers } from "../../data/constants";
 
 const SpeakerItem = ({ name, organization }) => {
   return (
-    <div className='col-6 col-lg-3 mb-4'>
-      <div className='card rounded-0'>
+    <div className="col-6 col-lg-3 mb-4">
+      <div className="card rounded-0">
         <a
-          href='#modal-speaker-1'
-          data-bs-toggle='modal'
-          data-bs-target='#modal-speaker-1'
+          href="#modal-speaker-1"
+          data-bs-toggle="modal"
+          data-bs-target="#modal-speaker-1"
         >
           {/* <img
             src='assets/images/speakers/speaker-2.jpg'
@@ -18,10 +18,10 @@ const SpeakerItem = ({ name, organization }) => {
             alt=''
           /> */}
         </a>
-        <div className='card-body'>
-          <h5 className='card-title mb-2'>{name}</h5>
-          <div className='card-text mb-3'>
-            <div className='meta'>{organization}</div>
+        <div className="card-body">
+          <h5 className="card-title mb-2">{name}</h5>
+          <div className="card-text mb-3">
+            <div className="meta">{organization}</div>
           </div>
         </div>
       </div>
@@ -30,17 +30,58 @@ const SpeakerItem = ({ name, organization }) => {
 };
 
 const SpeakersSection = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredSpeakers, setFilteredSpeakers] = useState([]);
+
+  const handleSearch = () => {
+    const filtered = speakers.filter((speaker) =>
+      speaker.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredSpeakers(filtered);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <>
       {/* <!-- Speakers Images --> */}
-      <section id='speakers-section' className='speakers-section section'>
-        <div className='container'>
-          <h3 className='section-heading text-center mb-3'>Speakers</h3>
-          <div className='section-intro text-center single-col-max mx-auto mb-5'>
+      <section id="speakers-section" className="speakers-section section">
+        <div className="container">
+          <h3 className="section-heading text-center mb-3">Speakers</h3>
+          <div className="section-intro text-center single-col-max mx-auto mb-5">
             Our Curated list of Speakers for the Pwani Innovation Week 2023
           </div>
-          <div className='row'>
-            {speakers.length > 0 ? (
+
+          <div className="speakers-cta text-center py-3 d-flex" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <input
+              type="text" style={{ width: '300px', height: '50px', padding: '5px' }}
+              className="form-control"
+              placeholder="Search Speakers"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)} onKeyDown={handleKeyDown}
+            />
+
+            <div>
+              <button className="btn btn-primary btn-lg" type="button" onClick={() =>handleSearch()}>
+                Search
+              </button>
+            </div>
+          </div>
+
+          <div className="row">
+            {searchQuery ? (
+              filteredSpeakers.map((speaker) => (
+                <SpeakerItem
+                  key={speaker.id}
+                  name={speaker.name}
+                  organization={speaker.organization}
+                />
+              ))
+            ) : speakers.length > 0 ? (
               speakers.map((speaker) => (
                 <SpeakerItem
                   key={speaker.id}
@@ -49,16 +90,16 @@ const SpeakersSection = () => {
                 />
               ))
             ) : (
-              <h4 className='text-center py-5 text-muted'>
-                Our Speaker List is getting Ready, we will update soon!
+              <h4 className="text-center py-5 text-muted">
+                Our Speaker List is getting ready; we will update soon!
               </h4>
             )}
           </div>
           {/* <!--//row--> */}
-          <div className='speakers-cta text-center py-3'>
+          <div className="speakers-cta text-center py-3">
             <a
-              className='btn btn-primary btn-lg'
-              href='https://afritickets.events/2270-pwani-innovation-week'
+              className="btn btn-primary btn-lg"
+              href="https://afritickets.events/2270-pwani-innovation-week"
             >
               Get Tickets
             </a>
@@ -67,7 +108,7 @@ const SpeakersSection = () => {
         {/* <!--//container--> */}
       </section>
       {/* <!--//speakers-section--> */}
-      <div className='container'>
+      <div className="container">
         <hr />
       </div>
     </>
